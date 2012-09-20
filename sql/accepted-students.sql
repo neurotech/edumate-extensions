b27722524525935c39f742bac161b6081948be2c
Tim Douglas <neurotech@gmail.com>
2012-09-20 16:21:36 +1000

    Added conditional dropdown support for choosing Starting Year and Year Group.

--- sql/accepted-students.sql
+++ sql/accepted-students.sql
@@ -1,15 +1,25 @@
-SELECT
-	contact.firstname as "First Name",
-	contact.surname as "Surname",
-	date_application as "Date of Application",
-	exp_form_run as "Expected Year and Form"
+WITH accepted_enrolments AS
+(
+	SELECT
+		contact.firstname as "First Name",
+		contact.surname as "Surname",
+		TO_CHAR(DATE(current_date), 'YYYY') AS "Current Year",
+		date_application as "Date of Application",
+		TO_CHAR(DATE(start_date), 'YYYY') AS "Start Year",
+		exp_form_run as "Expected Year and Form"
+	
+	FROM table(edumate.getallstudentstatus(current_date)) accepted
+	
+	INNER JOIN contact on accepted.contact_id = contact.contact_id
+	
+	WHERE
+		student_status_id = '6'
+	
+	ORDER BY
+		exp_form_run ASC, Surname ASC
+)
 
-FROM table(edumate.getallstudentstatus(current_date)) accepted
-
-INNER JOIN contact on accepted.contact_id = contact.contact_id
-
+SELECT *
+FROM accepted_enrolments
 WHERE
-	student_status_id = '6'
-
-ORDER BY
-	exp_form_run ASC, Surname ASC
+"Expected Year and Form" = '[[Starting Year and Cohort=query_list(SELECT form_run.form_run FROM form_run WHERE form_run >= '2012 %' ORDER BY form_run)]]'
