166691c33978454ea46c7d6a7d02c9185a17f577
Tim Douglas <neurotech@gmail.com>
2012-09-21 09:29:07 +1000

    Tweaked Accepted Students SQL.

--- sql/accepted-students.sql
+++ sql/accepted-students.sql
@@ -1,11 +1,9 @@
-WITH accepted_enrolments AS
+WITH accepted_applications AS
 (
 	SELECT
 		contact.firstname as "First Name",
 		contact.surname as "Surname",
-		TO_CHAR(DATE(current_date), 'YYYY') AS "Current Year",
 		date_application as "Date of Application",
-		TO_CHAR(DATE(start_date), 'YYYY') AS "Start Year",
 		exp_form_run as "Expected Year and Form"
 	
 	FROM table(edumate.getallstudentstatus(current_date)) accepted
@@ -20,6 +18,5 @@ WITH accepted_enrolments AS
 )
 
 SELECT *
-FROM accepted_enrolments
-WHERE
-"Expected Year and Form" = '[[Starting Year and Cohort=query_list(SELECT form_run.form_run FROM form_run WHERE form_run >= '2012 %' ORDER BY form_run)]]'
+FROM accepted_applications
+WHERE "Expected Year and Form" = '[[Starting Year and Cohort=query_list(SELECT form_run.form_run FROM form_run WHERE form_run >= '2012 %' ORDER BY form_run)]]'
