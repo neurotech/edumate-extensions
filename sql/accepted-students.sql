160285c52b97e12877ea58c0299bd3e00d9229aa
Tim Douglas <neurotech@gmail.com>
2012-09-21 12:27:04 +1000

    Added Total Students count to Accepted Students.

--- sql/accepted-students.sql
+++ sql/accepted-students.sql
@@ -26,7 +26,8 @@ gender_counts AS
 	SELECT
 		exp_form_run,
         SUM(CASE WHEN gender='Male' THEN 1 ELSE 0 END) AS "MALES",
-        SUM(CASE WHEN gender='Female' THEN 1 ELSE 0 END) AS "FEMALES"
+        SUM(CASE WHEN gender='Female' THEN 1 ELSE 0 END) AS "FEMALES",
+        count(exp_form_run) AS "TOTAL_STUDENTS"
 	FROM accepted_applications
 	GROUP BY exp_form_run
 )
@@ -36,9 +37,11 @@ SELECT
 	accepted_applications.surname,
 	accepted_applications.gender,
 	CAST(gender_counts.males AS VARCHAR(3))||' Boys, '||CAST(gender_counts.females AS VARCHAR(3))||' Girls.' AS "GENDER_COUNTS",
+	CAST(gender_counts.total_students AS VARCHAR(3))||' total students.' AS "TOTAL_STUDENTS",
 	accepted_applications.exp_form_run,
 	accepted_applications.external_school
 	
 FROM accepted_applications
 INNER JOIN gender_counts ON gender_counts.exp_form_run = accepted_applications.exp_form_run
-WHERE accepted_applications.exp_form_run = '[[Starting Year and Cohort=query_list(SELECT form_run.form_run FROM form_run WHERE form_run >= '2012 %' ORDER BY form_run)]]'
+WHERE accepted_applications.exp_form_run = '2014 Year 07'
+--WHERE accepted_applications.exp_form_run = '[[Starting Year and Cohort=query_list(SELECT form_run.form_run FROM form_run WHERE form_run >= '2012 %' ORDER BY form_run)]]'
