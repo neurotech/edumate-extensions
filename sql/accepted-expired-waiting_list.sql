005135c2631b177d00f3fc8f6375c6c56b316d94
Tim Douglas <neurotech@gmail.com>
2012-11-28 14:43:37 +1100

    Added form run as well as human-friendly STATUS logic

--- sql/accepted-expired-waiting_list.sql
+++ sql/accepted-expired-waiting_list.sql
@@ -3,7 +3,12 @@ SELECT
 	contact.firstname,
 	gender.gender,
 	contact.birthdate,
-	student_status_id,
+	CASE student_status_id
+		WHEN 6 THEN 'Place Accepted'
+		WHEN 14 THEN 'Expired Offer'
+		WHEN 9 THEN 'Wait Listed'
+		END AS "STATUS",
+	form_run,
 	priority,
 	TO_CHAR(futurekids.date_application,'DD/MM/YY') AS "APPLIED"
 
@@ -12,6 +17,7 @@ FROM table(edumate.getallstudentstatus(current_date)) futurekids
 INNER JOIN contact on contact.contact_id = futurekids.contact_id
 INNER JOIN gender on gender.gender_id = contact.gender_id
 INNER JOIN priority on priority.priority_id = futurekids.priority_id
+INNER JOIN form_run ON form_run.form_run_id = futurekids.exp_form_run_id
 
 WHERE student_status_id IN (6, 14, 9)
 
