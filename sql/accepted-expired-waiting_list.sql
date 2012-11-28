b1c172ec401385983e4826b3fbe3653c6dab86fc
Tim Douglas <neurotech@gmail.com>
2012-11-28 14:27:10 +1100

    More readable results

--- sql/accepted-expired-waiting_list.sql
+++ sql/accepted-expired-waiting_list.sql
@@ -1,5 +1,18 @@
-SELECT *
+SELECT
+	contact.surname,
+	contact.firstname,
+	gender.gender,
+	contact.birthdate,
+	student_status_id,
+	priority,
+	TO_CHAR(futurekids.date_application,'DD/MM/YY') AS "APPLIED"
 
-FROM table(edumate.getallstudentstatus(current_date))
+FROM table(edumate.getallstudentstatus(current_date)) futurekids
+
+INNER JOIN contact on contact.contact_id = futurekids.contact_id
+INNER JOIN gender on gender.gender_id = contact.gender_id
+INNER JOIN priority on priority.priority_id = futurekids.priority_id
 
 WHERE student_status_id IN (6, 14, 9)
+
+ORDER BY priority ASC
