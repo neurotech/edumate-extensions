13a3f6f57dcc4beb4be0142f4e0e17dd1678f42d
Tim Douglas <neurotech@gmail.com>
2012-11-28 12:20:49 +1100

    Finished current students house report

--- sql/current-students_house-information.sql
+++ sql/current-students_house-information.sql
@@ -1,14 +1,14 @@
 SELECT
+	form_run.form_run,
 	contact.firstname,
 	contact.surname,
-	form_run.form_run
+	house.house
 
 FROM table(edumate.get_enroled_students_form_run(current_date)) currents
 
 INNER JOIN form_run on form_run.form_run_id = currents.form_run_id
 INNER JOIN student on student.student_id = currents.student_id
 INNER JOIN contact on contact.contact_id = student.contact_id
-
-WHERE form_run LIKE '2012%'
+INNER JOIN house on house.house_id = student.house_id
 
 ORDER BY form_run, contact.surname
