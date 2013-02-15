4a16d9ac19da44da83c1b4d78d4c1e241f6000ca
Tim Douglas <neurotech@gmail.com>
2013-02-15 16:25:37 +1100

    Expanded scope

--- sql/enrolment/accepted-expired-waiting_list.sql
+++ sql/enrolment/accepted-expired-waiting_list.sql
@@ -5,15 +5,30 @@ WITH aew AS
 		contact.firstname,
 		gender.gender,
 		contact.birthdate,
+		
 		CASE student_status_id
 			WHEN 6 THEN 'Place Accepted'
 			WHEN 14 THEN 'Expired Offer'
 			WHEN 9 THEN 'Wait Listed'
 			WHEN 7 THEN 'Offered Place'
-			END AS "STATUS",
+			WHEN 10 THEN 'Application Received'
+		END AS "STATUS",
+		
 		form_run,
 		priority,
-		priority_level,
+		
+		CASE priority.priority_level
+			WHEN 0 THEN '0 - Partial Acceptance'
+			WHEN 1 THEN '1 - Current Families'
+			WHEN 2 THEN '2 - Italian Bilingual School'
+			WHEN 3 THEN '3 - Ex Student'
+			WHEN 4 THEN '4 - Catholic Student & School'
+			WHEN 5 THEN '5 - Catholic Student - Other'
+			WHEN 6 THEN '6 - Other'
+			WHEN 10 THEN '10 - Did not apply'
+			ELSE '999 - Empty'
+		END AS "PRIORITY_LEVEL_FULL",
+		
 		TO_CHAR(futurekids.date_application,'DD/MM/YY') AS "APPLIED",
 		next_interview,
 		date_offer
@@ -22,10 +37,10 @@ WITH aew AS
 	
 	INNER JOIN contact on contact.contact_id = futurekids.contact_id
 	INNER JOIN gender on gender.gender_id = contact.gender_id
-	INNER JOIN priority on priority.priority_id = futurekids.priority_id
+	FULL JOIN priority on priority.priority_id = futurekids.priority_id
 	INNER JOIN form_run ON form_run.form_run_id = futurekids.exp_form_run_id
 	
-	WHERE student_status_id IN (6, 14, 9, 7)
+	WHERE student_status_id IN (6, 14, 9, 7, 10)
 ),
 
 gender_counts AS
@@ -48,7 +63,7 @@ SELECT
 	aew.birthdate,
 	aew.status,
 	aew.form_run,
-	aew.priority,
+	aew.PRIORITY_LEVEL_FULL,
 	aew.applied,
 	aew.next_interview AS "INTERVIEW",
 	aew.date_offer AS "DATE_OF_OFFER"
@@ -57,4 +72,6 @@ FROM aew
 
 FULL JOIN gender_counts ON gender_counts.form_run = aew.form_run
 
-ORDER BY aew.status, aew.priority_level ASC
+WHERE aew.form_run NOT LIKE TO_CHAR((current_date),'YYYY') || ' Year %'
+
+ORDER BY aew.form_run ASC, aew.PRIORITY_LEVEL_FULL ASC, aew.status ASC, aew.surname ASC
