83eba1c66c1b9c75469f8f73a477da0ff95c69ee
Tim Douglas <neurotech@gmail.com>
2013-08-08 11:31:09 +1000

    Tidy up - Removed old Future Students Carer Addresses report

--- sql/enrolment/future-students_carer-addresses.sql
+++ sql/enrolment/future-students_carer-addresses.sql
@@ -1,14 +1,20 @@
-WITH future_students_carer_addresses AS
+WITH future_students_offered_addresses AS
 (
 	SELECT
 		contact.firstname,
 		contact.surname,
 		gender.gender,
 		exp_form_run,
-		external_school.external_school,
-		CASE student_status_id WHEN 6 THEN 'Place Accepted' ELSE 'Expired Offer' END AS "STATUS",
-		contact.email_address,
-	    PROPERTY_NAME, 
+		CASE student_status_id
+		  WHEN 6 THEN 'Place Accepted'
+		  WHEN 7 THEN 'Offered Place'
+			WHEN 8 THEN 'Interview Pending'
+			WHEN 9 THEN 'Wait Listed'
+			WHEN 10 THEN 'Application Received'
+			WHEN 14 THEN 'Expired Offer'
+			ELSE 'Other'
+		END AS "STATUS",
+		priority.priority,
 	    ADDRESS1 ||''|| ADDRESS2 AS STREET, 
 	    ADDRESS3 AS SUBURB, 
 	    COUNTRY,
@@ -26,9 +32,11 @@ WITH future_students_carer_addresses AS
 	INNER JOIN contact on accepted.contact_id = contact.contact_id
 	INNER JOIN gender on contact.gender_id = gender.gender_id
 	INNER JOIN stu_enrolment on accepted.student_id = stu_enrolment.student_id
-	INNER JOIN external_school on stu_enrolment.prev_school_id = external_school.external_school_id
 
     INNER JOIN form_run on accepted.exp_form_run_id = form_run.form_run_id
+    
+    LEFT JOIN priority ON priority.priority_id = accepted.priority_id
+    
     LEFT JOIN view_contact_home_address vcha on contact.contact_id = vcha.contact_id
     LEFT JOIN view_student_liveswith_carers vslc on stu_enrolment.student_id = vslc.student_id
 
@@ -38,7 +46,12 @@ WITH future_students_carer_addresses AS
     LEFT JOIN contact carer4 on vslc.carer4_contact_id = carer4.contact_id
 	
 	WHERE
-		student_status_id = '6' or student_status_id = '14'
+	  student_status_id = '6' OR
+	  student_status_id = '7' OR
+		student_status_id = '8' OR
+		student_status_id = '9' OR
+		student_status_id = '10' OR
+		student_status_id = '14'
 	
 	ORDER BY
 		exp_form_run ASC, surname ASC
@@ -51,40 +64,40 @@ gender_counts AS
         SUM(CASE WHEN gender='Male' THEN 1 ELSE 0 END) AS "MALES",
         SUM(CASE WHEN gender='Female' THEN 1 ELSE 0 END) AS "FEMALES",
         count(exp_form_run) AS "TOTAL_STUDENTS"
-	FROM future_students_carer_addresses
+	FROM future_students_offered_addresses
 	GROUP BY exp_form_run
 )
 
 SELECT
-	future_students_carer_addresses.firstname,
-	future_students_carer_addresses.surname,
-	future_students_carer_addresses.gender,
+	future_students_offered_addresses.firstname,
+	future_students_offered_addresses.surname,
+	future_students_offered_addresses.gender,
 	CAST(gender_counts.males AS VARCHAR(3))||' Boys, '||CAST(gender_counts.females AS VARCHAR(3))||' Girls' AS "GENDER_COUNTS",
 	CAST(gender_counts.total_students AS VARCHAR(3))||' total students' AS "TOTAL_STUDENTS",
-	future_students_carer_addresses.exp_form_run,
-	future_students_carer_addresses.external_school,
-	future_students_carer_addresses.STATUS,
-	future_students_carer_addresses.email_address,
-	future_students_carer_addresses.property_name,
-	future_students_carer_addresses.street,
-	future_students_carer_addresses.suburb,
-	future_students_carer_addresses.country,
-	future_students_carer_addresses.parent_titles,
-	future_students_carer_addresses.parent_firstnames,
-	future_students_carer_addresses.CARER1_FIRSTNAME,
-	future_students_carer_addresses.CARER1_SURNAME,
-	future_students_carer_addresses.CARER1_EMAIL_ADDRESS,
-	future_students_carer_addresses.CARER2_FIRSTNAME,
-	future_students_carer_addresses.CARER2_SURNAME,
-	future_students_carer_addresses.CARER2_EMAIL_ADDRESS,
-	future_students_carer_addresses.CARER3_FIRSTNAME,
-	future_students_carer_addresses.CARER3_SURNAME,
-	future_students_carer_addresses.CARER3_EMAIL_ADDRESS,
-	future_students_carer_addresses.CARER4_FIRSTNAME,
-	future_students_carer_addresses.CARER4_SURNAME,
-	future_students_carer_addresses.CARER4_EMAIL_ADDRESS
+	future_students_offered_addresses.exp_form_run,
+	future_students_offered_addresses.status,
+	future_students_offered_addresses.priority,
+	future_students_offered_addresses.street,
+	future_students_offered_addresses.suburb,
+	future_students_offered_addresses.country,
+	future_students_offered_addresses.parent_titles,
+	future_students_offered_addresses.parent_firstnames,
+	future_students_offered_addresses.CARER1_FIRSTNAME,
+	future_students_offered_addresses.CARER1_SURNAME,
+	future_students_offered_addresses.CARER1_EMAIL_ADDRESS,
+	future_students_offered_addresses.CARER2_FIRSTNAME,
+	future_students_offered_addresses.CARER2_SURNAME,
+	future_students_offered_addresses.CARER2_EMAIL_ADDRESS,
+	future_students_offered_addresses.CARER3_FIRSTNAME,
+	future_students_offered_addresses.CARER3_SURNAME,
+	future_students_offered_addresses.CARER3_EMAIL_ADDRESS,
+	future_students_offered_addresses.CARER4_FIRSTNAME,
+	future_students_offered_addresses.CARER4_SURNAME,
+	future_students_offered_addresses.CARER4_EMAIL_ADDRESS
 
-FROM future_students_carer_addresses
-INNER JOIN gender_counts ON gender_counts.exp_form_run = future_students_carer_addresses.exp_form_run
+FROM future_students_offered_addresses
+INNER JOIN gender_counts ON gender_counts.exp_form_run = future_students_offered_addresses.exp_form_run
 
-WHERE future_students_carer_addresses.exp_form_run = '[[Starting Year and Cohort=query_list(SELECT form_run.form_run FROM form_run WHERE form_run >= '2013 %' ORDER BY form_run)]]'
+WHERE future_students_offered_addresses.exp_form_run = '[[Starting Year and Cohort=query_list(SELECT form_run.form_run FROM form_run WHERE form_run >= '2013 %' ORDER BY form_run)]]'
+
+ORDER BY surname, firstname
