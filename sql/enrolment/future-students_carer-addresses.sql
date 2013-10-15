453d88159543e1a868821eb5bba0de26e2b9b51c
Tim Douglas <neurotech@gmail.com>
2013-10-15 14:15:02 +1100

    Added conditional logic to show home address when postal address is null

--- sql/enrolment/future-students_carer-addresses.sql
+++ sql/enrolment/future-students_carer-addresses.sql
@@ -1,7 +1,7 @@
 -- Future Students (Accepted/Interview Pending/Wait Listed/Application Received) - Addresses
 
 -- This report is a modified clone of the 'future-students_carer-addresses.sql' report.
--- It will render the following information for a specified form & year:
+-- It will render the following information for a specified form and year:
 
 -- 1. Student Firstname and Lastname
 -- 2. Gender, Gender Counts, Total Student Counts
@@ -33,17 +33,17 @@ WITH future_students_offered_addresses AS
 			ELSE 'Other'
 		END AS "STATUS",
 		priority.priority,
-	    ADDRESS1 ||''|| ADDRESS2 AS STREET, 
-	    ADDRESS3 AS SUBURB, 
-	    COUNTRY,
+	  (CASE WHEN VCPA.ADDRESS1 IS NULL THEN VCHA.ADDRESS1 || '' || VCHA.ADDRESS2 ELSE VCPA.ADDRESS1 || '' || VCPA.ADDRESS2 END) AS "STREET",
+	  (CASE WHEN VCPA.ADDRESS3 IS NULL THEN VCHA.ADDRESS3 ELSE VCPA.ADDRESS3 END) AS "SUBURB",
+    (CASE WHEN VCPA.COUNTRY IS NULL THEN VCHA.COUNTRY ELSE VCPA.COUNTRY END) AS "COUNTRY",
 
-	    vslc.SALUTATION as PARENT_TITLES,
-	    vslc.FIRSTNAMES as PARENT_FIRSTNAMES,
+	  vslc.SALUTATION as PARENT_TITLES,
+	  vslc.FIRSTNAMES as PARENT_FIRSTNAMES,
 	    
-	    carer1.firstname AS CARER1_FIRSTNAME, carer1.surname AS CARER1_SURNAME, carer1.email_address AS CARER1_EMAIL_ADDRESS,
-	    carer2.firstname AS CARER2_FIRSTNAME, carer2.surname AS CARER2_SURNAME, carer2.email_address AS CARER2_EMAIL_ADDRESS,
-	    carer3.firstname AS CARER3_FIRSTNAME, carer3.surname AS CARER3_SURNAME, carer3.email_address AS CARER3_EMAIL_ADDRESS,
-	    carer4.firstname AS CARER4_FIRSTNAME, carer4.surname AS CARER4_SURNAME, carer4.email_address AS CARER4_EMAIL_ADDRESS
+	  carer1.firstname AS CARER1_FIRSTNAME, carer1.surname AS CARER1_SURNAME, carer1.email_address AS CARER1_EMAIL_ADDRESS,
+	  carer2.firstname AS CARER2_FIRSTNAME, carer2.surname AS CARER2_SURNAME, carer2.email_address AS CARER2_EMAIL_ADDRESS,
+	  carer3.firstname AS CARER3_FIRSTNAME, carer3.surname AS CARER3_SURNAME, carer3.email_address AS CARER3_EMAIL_ADDRESS,
+	  carer4.firstname AS CARER4_FIRSTNAME, carer4.surname AS CARER4_SURNAME, carer4.email_address AS CARER4_EMAIL_ADDRESS
 
 	FROM table(edumate.getallstudentstatus(current_date)) accepted
 	
@@ -56,6 +56,7 @@ WITH future_students_offered_addresses AS
     LEFT JOIN priority ON priority.priority_id = accepted.priority_id
     
     LEFT JOIN view_contact_home_address vcha on contact.contact_id = vcha.contact_id
+    LEFT JOIN view_contact_postal_address vcpa on contact.contact_id = vcpa.contact_id
     LEFT JOIN view_student_liveswith_carers vslc on stu_enrolment.student_id = vslc.student_id
 
     LEFT JOIN contact carer1 on vslc.carer1_contact_id = carer1.contact_id
@@ -116,6 +117,7 @@ SELECT
 FROM future_students_offered_addresses
 INNER JOIN gender_counts ON gender_counts.exp_form_run = future_students_offered_addresses.exp_form_run
 
-WHERE future_students_offered_addresses.exp_form_run = '[[Starting Year and Cohort=query_list(SELECT form_run.form_run FROM form_run WHERE form_run >= '2013 %' ORDER BY form_run)]]'
+--WHERE future_students_offered_addresses.exp_form_run = '[[Starting Year and Cohort=query_list(SELECT form_run.form_run FROM form_run WHERE form_run >= '2013 %' ORDER BY form_run)]]'
+WHERE future_students_offered_addresses.exp_form_run = '2014 Year 07'
 
 ORDER BY surname, firstname
