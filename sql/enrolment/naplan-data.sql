ddd073c226f5247c7811740d7265ebb2705e9b31
Tim Douglas <neurotech@gmail.com>
2014-02-17 09:09:16 +1100

    Added country of birth, language spoken at home, name sorting

--- sql/enrolment/naplan-data.sql
+++ sql/enrolment/naplan-data.sql
@@ -14,7 +14,9 @@ SELECT
     WHEN gender.gender = 'Female' THEN '2'
     ELSE null
   END) "SEX",
-  TO_CHAR((contact.birthdate), 'DD/MM/YYYY') AS "DATE_OF_BIRTH"
+  TO_CHAR((contact.birthdate), 'DD/MM/YYYY') AS "DATE_OF_BIRTH",
+  birth_country.ascc_code AS "COUNTRY_OF_BIRTH",
+  language.ascl_id AS "LANG_AT_HOME"
 
 FROM TABLE(EDUMATE.get_currently_enroled_students((SELECT "NAPLAN_DAY" FROM report_vars))) gces
 
@@ -23,7 +25,9 @@ INNER JOIN contact ON contact.contact_id = student.contact_id
 INNER JOIN view_student_form_run vsfr ON vsfr.student_id = gces.student_id
 INNER JOIN form ON form.form_id = vsfr.form_id
 INNER JOIN gender ON gender.gender_id = contact.gender_id
+INNER JOIN country birth_country ON birth_country.country_id = student.birth_country_id
+INNER JOIN language ON language.language_id = contact.language_id
 
 WHERE form.short_name IN (7,9)
 
-ORDER BY form.short_name
+ORDER BY form.short_name, contact.surname, contact.preferred_name, contact.firstname
