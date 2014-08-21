89a09311b7ebd3dba210d94caca105ba9b5627a9
Tim Douglas <neurotech@gmail.com>
2014-08-21 15:39:08 +1000

    Expanded scope of portal importer

--- sql/admin/student-portal-import-generator.sql
+++ sql/admin/student-portal-import-generator.sql
@@ -6,4 +6,21 @@ SELECT
   
 FROM TABLE(EDUMATE.getallstudentstatus(current date)) gass
 
-WHERE gass.contact_id IN (SELECT contact_id FROM TABLE(edumate.getFormRunContactInfo(885, (current date))))
+WHERE gass.contact_id IN (
+  SELECT contact_id FROM TABLE(edumate.getFormRunContactInfo(883, (current date)))
+  UNION ALL
+  SELECT contact_id FROM TABLE(edumate.getFormRunContactInfo(884, (current date)))
+  UNION ALL
+  SELECT contact_id FROM TABLE(edumate.getFormRunContactInfo(886, (current date)))
+  UNION ALL
+  SELECT contact_id FROM TABLE(edumate.getFormRunContactInfo(887, (current date)))
+  UNION ALL
+  SELECT contact_id FROM TABLE(edumate.getFormRunContactInfo(888, (current date)))
+)
+
+
+-- 7: 883
+-- 8: 884
+-- 10: 886
+-- 11: 887
+-- 12: 888
