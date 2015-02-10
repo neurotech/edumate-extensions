0ca3a66ff028003da21eb38c2102c7c25a8d4ab4
Tim Douglas <neurotech@gmail.com>
2015-02-10 15:01:25 +1100

    Student Event Attendance changes
    Included attendance status

--- sql/attendance/student-event-attendance.sql
+++ sql/attendance/student-event-attendance.sql
@@ -28,13 +28,20 @@ WITH event_date_students AS
     SELECT
         lesson.date_on,
         attendance.student_id,
+        daily_attendance_status.daily_attendance_status,
         SUM(CASE WHEN lesson.period_class_id is not null AND NOT (attendance.attend_status_id = 1 OR attendance.attend_status_id is null) THEN 1 ELSE 0 END) AS HOMEROOMS,
         SUM(CASE WHEN attendance.attend_status_id = 3 AND lesson.period_class_id is not null THEN 1 ELSE 0 END) AS HR_ABSENT,
         SUM(CASE WHEN lesson.event_id is not null AND NOT (attendance.attend_status_id = 1 OR attendance.attend_status_id is null) THEN 1 ELSE 0 END) AS EVENTS,
         SUM(CASE WHEN attendance.attend_status_id = 3 AND lesson.event_id is not null THEN 1 ELSE 0 END) AS EV_ABSENT,
         MAX(event.event_id) AS EVENT_ID
+
     FROM event_date_students
+
         INNER JOIN attendance ON attendance.student_id = event_date_students.student_id
+        LEFT JOIN daily_attendance ON daily_attendance.student_id = event_date_students.student_id
+            AND daily_attendance.date_on = event_date_students.date_on
+        LEFT JOIN daily_attendance_status ON daily_attendance_status.daily_attendance_status_id = daily_attendance.daily_attendance_status_id
+
         INNER JOIN lesson ON lesson.lesson_id = attendance.lesson_id
             AND lesson.date_on = event_date_students.date_on
         LEFT JOIN period_class ON period_class.period_class_id = lesson.period_class_id
@@ -44,7 +51,7 @@ WITH event_date_students AS
         LEFT JOIN event_type ON event_type.event_type_id = event.event_type_id
             AND event_type.event_type LIKE '%eportable%'
     WHERE period.roll_flag = 1 OR event_type.event_type_id is not null
-    GROUP BY lesson.date_on, attendance.student_id
+    GROUP BY lesson.date_on, attendance.student_id, daily_attendance_status.daily_attendance_status
     ),  
 
     report_header AS
@@ -55,6 +62,8 @@ WITH event_date_students AS
         'FIRSTNAME' AS FIRSTNAME,
         'SURNAME' AS SURNAME,
         'HOMEROOM' AS HOMEROOM,
+        'YEAR_GROUP' AS "FORM",
+        9999 AS "FORM_SORT_ORDER",
         ev1.event AS EV1,
         ev2.event AS EV2,
         ev3.event AS EV3,
@@ -90,7 +99,7 @@ WITH event_date_students AS
         date_on,
         CASE WHEN events > 0 THEN (CASE WHEN ev_absent > 0 THEN 'X' ELSE null END)
             WHEN homerooms > 0 AND hr_absent > 0 
-                THEN (CASE WHEN homerooms = hr_absent THEN 'x' WHEN hr_absent > 0 THEN 'pa' ELSE null END)
+                THEN (CASE WHEN homerooms = hr_absent THEN daily_attendance_status WHEN hr_absent > 0 THEN 'pa' ELSE null END)
                 ELSE '' END AS STATUS
     FROM event_day_attendance
     ),
@@ -100,9 +109,11 @@ WITH event_date_students AS
     SELECT
         1 AS SORT_ORDER,
         student.student_number,
-        contact.firstname,
+        (CASE WHEN contact.preferred_name IS null THEN contact.firstname ELSE contact.preferred_name END) AS "FIRSTNAME",
         contact.surname,
         view_student_class_enrolment.class AS HOMEROOM,
+        ('Year ' || (SELECT short_name FROM TABLE(edumate.get_student_active_form_run((reportable_students.student_id), (current date))) FETCH FIRST 1 ROW ONLY)) AS "FORM_RUN",
+        (SELECT form_id FROM TABLE(edumate.get_student_active_form_run((reportable_students.student_id), (current date))) FETCH FIRST 1 ROW ONLY) AS "FORM_SORT_ORDER",
         ab1.status AS EV1,
         ab2.status AS EV2,
         ab3.status AS EV3,
@@ -117,6 +128,7 @@ WITH event_date_students AS
         LEFT JOIN view_student_class_enrolment ON view_student_class_enrolment.student_id = student.student_id
             AND view_student_class_enrolment.class_type_id = 2
             AND current_date BETWEEN view_student_class_enrolment.start_date AND view_student_class_enrolment.end_date
+
         INNER JOIN reportable_events ev1 ON ev1.event_no = 1
         LEFT JOIN reportable_events ev2 ON ev2.event_no = 2
         LEFT JOIN reportable_events ev3 ON ev3.event_no = 3
@@ -141,12 +153,21 @@ WITH event_date_students AS
     SELECT * FROM event_absence_report
     )
 
+--SELECT * FROM event_day_attendance
+
 SELECT
-    student_number AS COL1,
-    firstname AS COL2,
-    surname AS COL3,
-    homeroom AS COL4,
-    EV1, EV2, EV3, EV4, EV5, EV6, EV7,
-    YTD
+  student_number AS COL1,
+  firstname AS COL2,
+  surname AS COL3,
+  REPLACE(homeroom, '&#039;', '') AS COL4,
+  form AS COL5,
+  EV1, EV2, EV3, EV4, EV5, EV6, EV7,
+  YTD
+
 FROM final_report
-ORDER BY sort_order, homeroom, surname, firstname
+
+-- Home room sort
+--ORDER BY sort_order, homeroom, surname, firstname
+
+-- Form sort
+ORDER BY sort_order, form_sort_order, homeroom, surname, firstname
