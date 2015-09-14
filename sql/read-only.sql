6eca0d8c6a0cf9ca2f6690179c3214335531d222
Tim Douglas <neurotech@gmail.com>
2015-09-14 12:05:16 +1000

    READ ONLY SQL

---
+++ sql/read-only.sql
@@ -0,0 +1,7 @@
+CREATE ROLE READONLY;
+GRANT SELECT ON DB2INST1.VIEW_NAME TO ROLE READONLY;
+/*
+   Create a new user in the operating system, e.g.:
+  adduser development
+*/
+GRANT ROLE READONLY TO USER DEVELOPMENT;
