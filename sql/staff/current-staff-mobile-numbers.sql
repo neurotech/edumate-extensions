8d778cc69c4dbc0152682d791537ded38e4672f9
Tim Douglas <neurotech@gmail.com>
2015-09-14 12:05:04 +1000

    Added null handling to Current Staff Mobile Numbers

--- sql/staff/current-staff-mobile-numbers.sql
+++ sql/staff/current-staff-mobile-numbers.sql
@@ -1,7 +1,7 @@
 SELECT 
   (CASE WHEN contact.preferred_name IS null THEN contact.firstname ELSE contact.preferred_name END) AS "FIRSTNAME",
   contact.surname,
-  contact.mobile_phone
+  (CASE WHEN contact.mobile_phone IS NULL THEN '-' ELSE REPLACE(contact.mobile_phone, ' ', '') END) AS MOBILE_PHONE
 
 FROM group_membership
 
