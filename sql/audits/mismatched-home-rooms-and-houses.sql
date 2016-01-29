fd33934e35ec0966da292c81ffcc3344fe3390fa
Tim Douglas <neurotech@gmail.com>
2016-01-29 14:35:22 +1100

    Updated mismatched home rooms and houses report to be useful

--- sql/audits/mismatched-home-rooms-and-houses.sql
+++ sql/audits/mismatched-home-rooms-and-houses.sql
@@ -14,6 +14,7 @@ WITH student_homerooms AS (
 hr_house AS (
   SELECT
     student.student_id,
+    student_homerooms.class AS "HR_FULL_NAME",
     LEFT(student_homerooms.class, (LENGTH(student_homerooms.class) - 14)) AS "HR",
     house.house
   
@@ -21,6 +22,23 @@ hr_house AS (
   
   INNER JOIN house ON house.house_id = student.house_id
   INNER JOIN student_homerooms ON student_homerooms.student_id = student.student_id
+),
+
+mismatched AS (
+  SELECT * FROM hr_house WHERE hr != house
 )
 
-SELECT student_id FROM hr_house WHERE hr != house
+SELECT
+  student.student_number,
+  COALESCE(contact.preferred_name, contact.firstname) || ' ' || contact.surname AS "STUDENT_NAME",
+  vsfr.form_run AS "YEAR_GROUP",
+  mismatched.hr_full_name AS "CURRENT_HOME_ROOM",
+  mismatched.house AS "CURRENT_HOUSE"
+
+FROM mismatched
+
+INNER JOIN view_student_form_run vsfr ON vsfr.student_id = mismatched.student_id AND academic_year = YEAR(current date)
+INNER JOIN student ON student.student_id = mismatched.student_id
+INNER JOIN contact ON contact.contact_id = student.contact_id
+
+ORDER BY vsfr.form_run, contact.surname, contact.preferred_name, contact.firstname, mismatched.hr_full_name, mismatched.house
