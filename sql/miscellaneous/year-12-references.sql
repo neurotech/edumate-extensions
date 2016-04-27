b7b0f0fe6cf7a483b985edc5f3a7dd928d45d19b
Tim Douglas <neurotech@gmail.com>
2016-04-27 13:16:54 +1000

    Added awards information to Year 12 References

--- sql/miscellaneous/year-12-references.sql
+++ sql/miscellaneous/year-12-references.sql
@@ -1,7 +1,7 @@
 WITH report_vars AS (
   SELECT
-    DATE('2014-10-18') AS "PRELIM_DATE",
-    DATE('2015-12-12') AS "SENIOR_DATE"
+    DATE('2015-10-06') AS "PRELIM_DATE",
+    DATE('2016-07-12') AS "SENIOR_DATE"
 
   FROM SYSIBM.SYSDUMMY1
 ),
@@ -112,12 +112,71 @@ senior_cr AS (
     course.code LIKE 'CR%'
   
   GROUP BY vsce.student_id
+),
+
+award_winners AS (
+  SELECT
+    student_welfare.student_id,
+    wh.what_happened_id,
+    course.course_id,
+    wh.what_happened || (CASE WHEN course.print_name = 'Home Room' THEN ' for ' || REPLACE(course.course, 'Home Room', '') ELSE ' in ' || course.print_name END) AS "AWARD"
+    --(CASE WHEN course.print_name = 'Home Room' THEN REPLACE(course.course, 'Home Room', '') ELSE course.print_name END) AS "COURSE"
+    
+  FROM student_welfare
+  
+  INNER JOIN what_happened wh ON wh.what_happened_id = student_welfare.what_happened_id
+  INNER JOIN class ON class.class_id = student_welfare.class_id
+  INNER JOIN course ON course.course_id = class.course_id
+  
+  WHERE
+    student_welfare.student_id IN (SELECT student_id FROM all_students)
+    AND
+    YEAR(student_welfare.date_entered) = YEAR(current date - 1 YEAR)
+    AND
+    /*
+      WHAT_HAPPENED_ID  |  WHAT_HAPPENED
+      ------------------|---------------------------------------------------
+      1                 |  Certificate of Merit
+      49                |  Letter of Commendation
+      ------------------|---------------------------------------------------
+      145               |  St Benedict Award: Leadership and Service
+      146               |  Leadership and Service Medallion
+      147               |  St Scholastica Award: College Dux
+      148               |  Academic Medallion
+      149               |  Archbishop of Sydney: Award for Excellence
+      150               |  Caltex Award
+      151               |  Pierre de Coubertin
+      152               |  Reuben F Scarf Award
+      153               |  ADF Long Tan Youth Leadership and Teamwork Award
+      ------------------|---------------------------------------------------
+      154               |  Academic Excellence
+      155               |  Academic Merit
+      156               |  Consistent Effort
+      ------------------|---------------------------------------------------
+      169               |  Good Samaritan Award
+      170               |  Leadership and Service Award
+      193               |  House Award
+    */
+    --student_welfare.what_happened_id IN (154, 155, 156)
+    --student_welfare.what_happened_id in (145, 146, 147, 148, 149, 150, 151, 152, 153)
+    --student_welfare.what_happened_id in (145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156)
+    student_welfare.what_happened_id in (145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 169, 170, 193)   
+),
+
+award_winners_agg AS (
+  SELECT
+    student_id,
+    LISTAGG(award, ', ') WITHIN GROUP(ORDER BY award) AS "AWARDS"
+
+  FROM award_winners
+  
+  GROUP BY student_id
 )
 
 SELECT
   student.student_number,
   contact.firstname,
-  contact.preferred_name,
+  COALESCE(contact.preferred_name, '') AS "PREFERRED_NAME",
   contact.surname,
   REPLACE(REPLACE(hr.class, '&#039;', ''''), ' Home Room ', ' ') AS "HOMEROOM",
   --contact.birthdate AS "DOB",
@@ -128,8 +187,9 @@ SELECT
   gass.end_date,
   gass.last_form_run AS "GRADUATING_FORM_RUN",
   senior_courses.courses AS "SENIOR_COURSES",
-  prelim_cr.courses AS "PRELIM_CC_REP",
-  senior_cr.courses AS "SENIOR_CC_REP"
+  COALESCE(prelim_cr.courses, '') AS "PRELIM_CC_REP",
+  COALESCE(senior_cr.courses, '') AS "SENIOR_CC_REP",
+  COALESCE(award_winners_agg.awards, '') AS "PRELIM_AWARDS"
 
 FROM all_students students
 
@@ -141,6 +201,7 @@ INNER JOIN class hr ON hr.class_id = vsce.class_id AND hr.class_type_id = 2 AND 
 LEFT JOIN senior_courses ON senior_courses.student_id = students.student_id
 LEFT JOIN prelim_cr ON prelim_cr.student_id = students.student_id
 LEFT JOIN senior_cr ON senior_cr.student_id = students.student_id
+LEFT JOIN award_winners_agg ON award_winners_agg.student_id = students.student_id
 
 WHERE gass.student_status_id = 5 AND gass.last_form_run_id = (SELECT form_run_id FROM form_run WHERE form_id = 14 AND form_run LIKE TO_CHAR((current date), 'YYYY') || '%')
 
