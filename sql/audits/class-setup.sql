616c73eb53f7ef8c5b5e6c446b7b8bd69c21f427
Tim Douglas <neurotech@gmail.com>
2016-04-27 13:07:54 +1000

    Added VET and Home Rooms to Class Setup audit

--- sql/audits/class-setup.sql
+++ sql/audits/class-setup.sql
@@ -1,10 +1,31 @@
-WITH cc AS (
+WITH home_rooms AS (
   SELECT
     '1' AS "SORT_ORDER",
     class_id,
     class,
-    class_type.class_type,
-    (SELECT class_type FROM class_type WHERE class_type_id = 4) AS "SHOULD_BE"
+    class_type.class_type AS "ACTUAL",
+    (SELECT class_type FROM class_type WHERE class_type_id = 2) AS "EXPECTED"
+  
+  FROM class
+  
+  INNER JOIN class_type ON class_type.class_type_id = class.class_type_id
+  INNER JOIN course ON course.course_id = class.course_id
+  
+  WHERE
+    academic_year_id = (SELECT academic_year_id FROM academic_year WHERE academic_year = YEAR(current date))
+    AND
+    class.class_type_id = 1
+    AND
+    course.code LIKE '%Home Room%'
+),
+
+cc AS (
+  SELECT
+    '2' AS "SORT_ORDER",
+    class_id,
+    class,
+    class_type.class_type AS "ACTUAL",
+    (SELECT class_type FROM class_type WHERE class_type_id = 4) AS "EXPECTED"
   
   FROM class
   
@@ -21,13 +42,40 @@ WITH cc AS (
     course.code LIKE 'CR%')
 ),
 
-lifeskills AS (
+vet AS (
   SELECT
-    '2' AS "SORT_ORDER",
+    '3' AS "SORT_ORDER",
     class_id,
     class,
-    class_type.class_type,
-    (SELECT class_type FROM class_type WHERE class_type_id = 10) AS "SHOULD_BE"
+    class_type.class_type AS "ACTUAL",
+    (SELECT class_type FROM class_type WHERE class_type_id = 9) AS "EXPECTED"
+    
+    FROM class
+    
+    INNER JOIN class_type ON class_type.class_type_id = class.class_type_id
+    INNER JOIN course ON course.course_id = class.course_id
+
+    WHERE
+      academic_year_id = (SELECT academic_year_id FROM academic_year WHERE academic_year = YEAR(current date))
+      AND
+      class.class_type_id = 1
+      AND
+      (LOWER(class.class) LIKE '%ports coaching%'
+      OR
+      LOWER(class.class) LIKE '%ospitality%'
+      OR
+      LOWER(class.class) LIKE '%usiness service%'
+      OR
+      LOWER(class.class) LIKE '%nformation and digita%')
+),
+
+lifeskills AS (
+  SELECT
+    '4' AS "SORT_ORDER",
+    class_id,
+    class,
+    class_type.class_type AS "ACTUAL",
+    (SELECT class_type FROM class_type WHERE class_type_id = 10) AS "EXPECTED"
   
   FROM class
   
@@ -45,10 +93,14 @@ lifeskills AS (
 combined AS (
   SELECT * FROM lifeskills
   UNION
+  SELECT * FROM vet
+  UNION
   SELECT * FROM cc
+  UNION
+  SELECT * FROM home_rooms
 )
 
-SELECT class_id, class, class_type, should_be
+SELECT class_id, class, actual, expected
 
 FROM combined
 
