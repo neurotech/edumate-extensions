6780e8b05ecf2f7fa57759471272f1ce38ceb50a
Tim Douglas <neurotech@gmail.com>
2016-05-11 14:25:05 +1000

    New VIEW: Co-Curricular Enrolments

---
+++ sql/_views/view_co_curricular_enrolments.sql
@@ -0,0 +1,38 @@
+CREATE OR REPLACE VIEW DB2INST1.VIEW_CO_CURRICULAR_ENROLMENTS (
+  SORT,
+  STUDENT_ID,
+  CLASS_ID,
+  IDENTIFIER,
+  TERM,
+  START_DATE,
+  END_DATE
+) AS
+
+SELECT * FROM (
+  SELECT
+    ROW_NUMBER() OVER (PARTITION BY class_enrollment.student_id ORDER BY class_enrollment.start_date ASC) AS "SORT",
+    class_enrollment.student_id,
+    class_enrollment.class_id,
+    class.identifier,
+    (CASE
+      WHEN class.identifier LIKE '01%' THEN RIGHT(LEFT(class.identifier, 2), 1)
+      WHEN class.identifier LIKE '02%' THEN RIGHT(LEFT(class.identifier, 2), 1)
+      WHEN class.identifier LIKE '02%' THEN RIGHT(LEFT(class.identifier, 2), 1)
+      WHEN class.identifier LIKE '03%' THEN RIGHT(LEFT(class.identifier, 2), 1)
+      WHEN class.identifier LIKE '00%' THEN RIGHT(class.identifier, 1)
+      ELSE '9'
+    END) AS "TERM",
+    class_enrollment.start_date,
+    class_enrollment.end_date
+  
+  FROM class_enrollment
+  
+  INNER JOIN class ON class.class_id = class_enrollment.class_id
+  
+  WHERE
+    class.class_type_id = 4
+    AND
+    (current date) BETWEEN start_date AND end_date
+  
+  ORDER BY student_id, start_date
+)
