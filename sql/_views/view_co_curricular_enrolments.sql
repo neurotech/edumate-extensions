72e1f6b6fe5d94abd0b24181405724f7b1728975
Tim Douglas <neurotech@gmail.com>
2016-05-13 09:12:37 +1000

    Added class_enrollment_id to Co-Curricular Enrolments view

--- sql/_views/view_co_curricular_enrolments.sql
+++ sql/_views/view_co_curricular_enrolments.sql
@@ -1,5 +1,6 @@
 CREATE OR REPLACE VIEW DB2INST1.VIEW_CO_CURRICULAR_ENROLMENTS (
   SORT,
+  CLASS_ENROLLMENT_ID,
   STUDENT_ID,
   CLASS_ID,
   IDENTIFIER,
@@ -11,6 +12,7 @@ CREATE OR REPLACE VIEW DB2INST1.VIEW_CO_CURRICULAR_ENROLMENTS (
 SELECT * FROM (
   SELECT
     ROW_NUMBER() OVER (PARTITION BY class_enrollment.student_id ORDER BY class_enrollment.start_date ASC) AS "SORT",
+    class_enrollment.class_enrollment_id,
     class_enrollment.student_id,
     class_enrollment.class_id,
     class.identifier,
