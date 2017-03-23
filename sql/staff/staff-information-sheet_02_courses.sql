cb619ed5eab5857e4dc6d05ee5dc4b5f57e43acf
Tim Douglas <neurotech@gmail.com>
2017-03-23 14:38:59 +1100

    Updated for 2017 requirements

--- sql/staff/staff-information-sheet_02_courses.sql
+++ sql/staff/staff-information-sheet_02_courses.sql
@@ -2,26 +2,50 @@ WITH TRAINING_COURSES_RAW AS
 (
   SELECT
       STAFF.STAFF_NUMBER,
-      STAFF_COURSE.DETAILS || ' (' || TO_CHAR(STAFF_COURSE.STARTDATE, 'DD Month YYYY') || ')' AS "DETAILS"
+      staff_course.staff_course AS "COURSE",
+      STAFF_COURSE.details,
+      staff_course.hours,
+      TO_CHAR(staff_course.startdate, 'DD Mon YYYY') AS "START_DATE",
+      TO_CHAR(staff_course.enddate, 'DD Mon YYYY') AS "END_DATE",
+      institute.institute,
+      STAFF_COURSE.startdate
   
   FROM STAFF_COURSE
   
   LEFT JOIN CONTACT ON CONTACT.CONTACT_ID = STAFF_COURSE.CONTACT_ID
   LEFT JOIN STAFF ON STAFF_COURSE.CONTACT_ID = STAFF.CONTACT_ID
+  LEFT JOIN institute ON institute.institute_id = staff_course.institute_id
   
-  WHERE CONTACT.CONTACT_ID = [[mainquery.contact_id]]
-    AND STARTDATE > (current_date - 1 YEAR)
+  WHERE
+    CONTACT.CONTACT_ID = [[mainquery.contact_id]]
     AND DETAILS IS NOT NULL
     AND STAFF_COURSE IS NOT NULL
-    AND DETAILS NOT LIKE '$%'
-    AND DETAILS NOT LIKE 'Free'
-    AND DETAILS NOT LIKE 'nil'
+),
+
+total_hours_this_year AS (
+  SELECT
+    staff_number,
+    SUM(hours) AS "HOURS"
+    
+  FROM training_courses_raw
+  
+  WHERE YEAR(startdate) = YEAR(current date)
+  
+  GROUP BY staff_number
 )
 
 SELECT
-  STAFF_NUMBER,
-  LISTAGG(DETAILS, ', ') WITHIN GROUP(ORDER BY STAFF_NUMBER) "ALL_COURSES"
+  TRAINING_COURSES_RAW.staff_number,
+  CHAR(COALESCE(total_hours_this_year.hours, 0)) AS "HOURS_THIS_YEAR",
+  training_courses_raw.course,
+  training_courses_raw.details,
+  CHAR(training_courses_raw.hours) AS "HOURS",
+  training_courses_raw.start_date,
+  training_courses_raw.end_date,
+  COALESCE(training_courses_raw.institute, '') AS "INSTITUTE"
 
 FROM TRAINING_COURSES_RAW
 
-GROUP BY STAFF_NUMBER
+LEFT JOIN total_hours_this_year ON total_hours_this_year.staff_number = training_courses_raw.staff_number
+
+ORDER BY startdate DESC
